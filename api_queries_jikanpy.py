534146ffbc3b08d1cfbee027ba67ee7d738634fb
seraphritt <isak369@hotmail.com>
2023-06-19 14:39:02 -0300

    adding queries (characters, animes, manga (synonym titles))

--- api_queries_jikanpy.py
+++ api_queries_jikanpy.py
@@ -1,10 +1,35 @@
 from jikanpy import Jikan
 jikan = Jikan()
 # available search queries
-# characters / animes / manga (episodes)
-result = jikan.search("characters", "Naruto")
-# se não houver resultado
-if result["pagination"]["items"]["count"] == 0:
-    print("No results found")
-else:
-    print(result["data"][0]['about'])
+# characters / animes / manga (nomes sinônimos)
+
+
+def query(command: str, arguments: list[str]) -> list[str]:
+    result = jikan.search(command, *arguments)
+    # se não houver resultado
+    if command.lower() == "characters":
+        if result["pagination"]["items"]["count"] == 0:
+            return []
+        else:
+            return result["data"][0]['about']
+    elif command.lower() == "anime":
+        if result["pagination"]["items"]["count"] == 0:
+            return []
+        else:
+            return result["data"][0]["synopsis"]
+    elif command.lower() == "manga":
+        if result["pagination"]["items"]["count"] == 0:
+            return []
+        else:
+            res = []
+            for each in range(len(result["data"][0]["titles"])):
+                res.append(result["data"][0]["titles"][each]["title"])
+            return res
+
+
+# query para personagem:
+# query("characters", ["naruto"])
+# query para anime:
+# query("anime", ["boruto"]))
+# query para título similar de manga:
+# query("manga", ["demon slayer"])
