183a815c470eb3f6fbee1535221a32116b19caa6
seraphritt <isak369@hotmail.com>
2023-06-20 20:37:35 -0300

    adding requirements.txt

--- api_queries_jikanpy.py
+++ api_queries_jikanpy.py
@@ -1,7 +1,16 @@
 from jikanpy import Jikan
+import re
 jikan = Jikan()
 # available search queries
-# characters / animes / manga (nomes sinônimos)
+# characters / animes
+
+
+def query_filter(query_result: str) -> str:
+    resu = re.sub(r'\[.*\n', "", query_result)  # remove o [Written by...]
+    resu = re.sub(r'\(.*\n', "", resu)  # remove o (Source ...)
+    resu = re.sub(r'.*:.*\n', "", resu)  # remove as informações adicionais do personagem
+    # informações adicionais como age, birthday...
+    return resu
 
 
 def query(command: str, arguments: list[str]) -> list[str]:
@@ -11,12 +20,12 @@ def query(command: str, arguments: list[str]) -> list[str]:
         if result["pagination"]["items"]["count"] == 0:
             return []
         else:
-            return result["data"][0]['about']
+            return [result["data"][0]["about"]]
     elif command.lower() == "anime":
         if result["pagination"]["items"]["count"] == 0:
             return []
         else:
-            return result["data"][0]["synopsis"]
+            return [result["data"][0]["synopsis"]]
     elif command.lower() == "manga":
         if result["pagination"]["items"]["count"] == 0:
             return []
@@ -27,9 +36,23 @@ def query(command: str, arguments: list[str]) -> list[str]:
             return res
 
 
+# a query é retornada em uma lista de strings
+
 # query para personagem:
-# query("characters", ["naruto"])
+# query("characters", ["Tengen Uzui"])[0]
 # query para anime:
-# query("anime", ["boruto"]))
-# query para título similar de manga:
-# query("manga", ["demon slayer"])
+# query("anime", ["Pokémon Pikachu"]))[0]
+
+# passando a query pelo filtro
+
+# query de anime
+
+# resultado = query("anime", ["Boruto"])[0]
+# resultado = query_filter(resultado)
+# print(resultado)
+# query de characters
+
+# resultado = query("characters", ["Sasuke"])[0]
+# resultado = query_filter(resultado)
+# print(resultado)
+[]
